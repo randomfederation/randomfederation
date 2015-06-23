0866002f4f2a6033c723e5729432eec8ad6eeb32
randomfederation <randomfederation@users.noreply.github.com>
2015-06-23 18:37:20 +0300

    Create dice.php

---
+++ dice.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Dice с президентом</title>
+	<link rel="stylesheet" href="styles/main.css">
+	<link rel="stylesheet" href="styles/dice.css">
+</head>
+<body>
+	<div class="wrapper center">
+		<div class="github">
+			<a href="https://github.com/randomfederation/randomfederation/blob/master/dice.php" class="github-link" title="Github сурс для dice.php" target="_blank"></a>
+		</div>
+		<header>
+			<a href="index.php"><img src="img/rand-fed-logo.png" alt="rand fed logo"></a>
+			<!-- <img src="http://i.imgur.com/SET67TJ.png" alt=""> -->
+		</header>
+		<main>
+			<h1>Dice с президентом</h1>
+			<h2>К сожалению, президент сейчас в командировке.</h2>
+		</main>
+		<footer>
+			<span class="copy">&copy <?php echo date('Y')?> Министерство развлечений Рандомной Федерации | Все права защищены</span>
+			<a href="index.php">Главная</a>
+			<a href="currency.php">Узнайте курс валют!</a>
+		</footer>
+	</div>
+</body>
+</html>
